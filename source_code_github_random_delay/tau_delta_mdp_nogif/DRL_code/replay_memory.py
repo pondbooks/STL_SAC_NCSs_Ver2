bee0db08a33182419e2b3a0b49e5798bccf17bcd
pondbooks <68591842+pondbooks@users.noreply.github.com>
2022-06-11 09:49:30 +0900

    Add files via upload

---
+++ source_code_github_random_delay/tau_delta_mdp_nogif/DRL_code/replay_memory.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+from torch import nn
+
+class ReplayBuffer:
+
+    def __init__(self, buffer_size, state_shape, action_shape, device):
+        self._p = 0
+        self._n = 0
+        self.buffer_size = buffer_size
+
+        self.states = torch.empty((buffer_size, *state_shape), dtype=torch.float, device=device)
+        self.actions = torch.empty((buffer_size, *action_shape), dtype=torch.float, device=device)
+        self.rewards = torch.empty((buffer_size, 1), dtype=torch.float, device=device)
+        self.dones = torch.empty((buffer_size, 1), dtype=torch.float, device=device)
+        self.next_states = torch.empty((buffer_size, *state_shape), dtype=torch.float, device=device)
+
+    def append(self, state, action, reward, done, next_state):
+        self.states[self._p].copy_(torch.from_numpy(state))
+        self.actions[self._p].copy_(torch.from_numpy(action))
+        self.rewards[self._p] = float(reward)
+        self.dones[self._p] = float(done)
+        self.next_states[self._p].copy_(torch.from_numpy(next_state))
+
+        self._p = (self._p + 1) % self.buffer_size
+        self._n = min(self._n + 1, self.buffer_size)
+
+    def sample(self, batch_size):
+        idxes = np.random.randint(low=0, high=self._n, size=batch_size)
+        return (
+            self.states[idxes],
+            self.actions[idxes],
+            self.rewards[idxes],
+            self.dones[idxes],
+            self.next_states[idxes]
+        )
---
+++ source_code_github_random_delay/tau_delta_mdp_nogif/gym_pathplan/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_pathplan.envs.stl_pathplan import STL_Problem, STL_Problem_Preprocess
